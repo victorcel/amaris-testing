eefdd460eb46432f1de179b27b7a0cf8f524e291
Victor Elias Barrera Florez <vbarrera@vozy.co>
2022-10-18 20:51:21 -0500

    feat: 2 Realizar una funcion que reciba un numero entero "id" de un pokemon y devuelva su nombre. Para esto la funcion debe consultar una api de pokemon enviarle el id y obtener el campo "nombre" el cual va a retornar. URL = https://pokeapi.co/api/v2/pokemon-form/{{id}}

--- main.go
+++ main.go
@@ -3,10 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/victorcel/amaris-testing/useCases"
+	"net/http"
 )
 
 func main() {
 
-	fmt.Println(useCases.OrderText("Luis,Camilo;Andres,Laura"))
+	//fmt.Println(useCases.OrderText("Luis,Camilo;Andres,Laura"))
+	usePokemon := useCases.NewPokemon(&http.Client{})
+	pokemon, err := usePokemon.GetPokemon(1111)
+	if err != nil {
+		fmt.Println(err)
+	}
 
+	fmt.Println(pokemon.ID)
 }
---
+++ useCases/connectApiPokemonUseCase_test.go
@@ -0,0 +1,95 @@
+package useCases
+
+import (
+	"fmt"
+	"github.com/jarcoal/httpmock"
+	"github.com/victorcel/amaris-testing/models"
+	"net/http"
+	"reflect"
+	"testing"
+)
+
+func TestPokemon_GetPokemon(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	httpmock.RegisterResponder("GET", fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-form/%d", 1),
+		func(req *http.Request) (*http.Response, error) {
+			resp, err := httpmock.NewJsonResponse(http.StatusOK, models.Episodio{
+				ID:   1,
+				Name: "Victor",
+			})
+			if err != nil {
+				return httpmock.NewStringResponse(http.StatusInternalServerError, ""), nil
+			}
+			return resp, nil
+		},
+	)
+
+	httpmock.RegisterResponder("GET", fmt.Sprintf("https://pokeapi.co/api/v2/pokemon-form/%d", 1111),
+		func(req *http.Request) (*http.Response, error) {
+			return httpmock.NewStringResponse(http.StatusInternalServerError, ""), nil
+		})
+
+	type fields struct {
+		client *http.Client
+	}
+
+	type args struct {
+		pokemonID int
+	}
+
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    models.Episodio
+		mocker  func(a args)
+		wantErr bool
+	}{
+		{
+			name: "TestGetPokemon success",
+			fields: fields{
+				client: &http.Client{},
+			},
+			args: args{
+				pokemonID: 1,
+			},
+			want: *&models.Episodio{
+				ID:   1,
+				Name: "Victor",
+			},
+			wantErr: false,
+		},
+		{
+			name: "TestGetPokemon error",
+			fields: fields{
+				client: &http.Client{},
+			},
+			args: args{
+				pokemonID: 1111,
+			},
+			want: *&models.Episodio{},
+
+			wantErr: true,
+		},
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Pokemon{
+				client: tt.fields.client,
+			}
+			got, err := p.GetPokemon(tt.args.pokemonID)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("GetPokemon() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("GetPokemon() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
